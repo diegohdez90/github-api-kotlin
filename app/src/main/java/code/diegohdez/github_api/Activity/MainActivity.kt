6002b388f027b4d2b3d5ddb47e894eeb1d557f4c
diegohdez12@gmail.com <diegohdez12@gmail.com>
2018-09-06 14:36:10 -0500

    initial commit
    add main and get token activities
    
    install Fuel networking library for kotlin

---
+++ app/src/main/java/code/diegohdez/github_api/Activity/MainActivity.kt
@@ -0,0 +1,13 @@
+package code.diegohdez.github_api.Activity
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import code.diegohdez.github_api.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/code/diegohdez/github_api/GithubApi.kt
@@ -0,0 +1,11 @@
+package code.diegohdez.github_api
+
+import android.app.Application
+import com.github.kittinunf.fuel.core.FuelManager
+
+class GithubApi : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        FuelManager.instance.basePath = "https://api.github.com/"
+    }
+}
